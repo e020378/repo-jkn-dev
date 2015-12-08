fc08fc46c9d39d53bff1179087b0720617330096
e020378 <e020378@gmail.com>
2015-12-08 08:30:00 +0100

    Initial Commit

---
+++ common-utils/src/main/java/org/jkn/app/commons/common_utils/App.java
@@ -0,0 +1,13 @@
+package org.jkn.app.commons.common_utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
